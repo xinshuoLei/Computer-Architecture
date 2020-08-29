525ce5a01ec0047530ad388257a890c07e94930c
Xinshuo <xinshuo3@illinois.edu>
2020-08-28 23:55:50 -0500

    finish count ones

--- Lab1/countOnes.cpp
+++ Lab1/countOnes.cpp
@@ -5,8 +5,10 @@
 
 unsigned countOnes(unsigned input) {
 	// TODO: write your code here
-
-	
-
-	return input;
+	unsigned twoBit = (input & 0x55555555) + ((input & 0xAAAAAAAA) >> 1);
+	unsigned fourBit = (twoBit& 0x33333333) + ((twoBit & 0xCCCCCCCC) >> 2);
+	unsigned eightBit = (fourBit & 0x0f0f0f0f) + ((fourBit & 0xf0f0f0f0) >> 4);
+	unsigned sixteenBit = (eightBit & 0x00ff00ff) + ((eightBit & 0xff00ff00) >> 8);
+	unsigned result = (sixteenBit & 0x0000ffff) + ((sixteenBit & 0xffff0000) >> 16);
+	return result;
 }
