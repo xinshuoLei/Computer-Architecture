8ba452132adc4773dbe6e3648400ceac55b953d9
Xinshuo Lei <xinshuo3@linux-a2.ews.illinois.edu>
2020-09-19 21:02:51 -0500

    test PC

--- Lab4/arith_machine.v
+++ Lab4/arith_machine.v
@@ -9,20 +9,24 @@ module arith_machine(except, clock, reset);
     input       clock, reset;
 
     wire [31:0] inst;  
-    wire [31:0] PC;  
+    wire [31:0] PC = 32'h0;
+    wire [31:0] nextPC = 32'h0;
+    wire [31:0] instruction;
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    register #(32) PC_reg( /* connect signals */ );
+    register #(32) PC_reg(PC, nextPC, clock, 1'b1, 1'b0);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    instruction_memory im( /* connect signals */ );
+    instruction_memory im(instruction, PC);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
     regfile rf ( /* connect signals */ );
 
     /* add other modules */
+    alu32 pcplus4(nextPC, , , , PC, 32'h4,`ALU_ADD);
+    assign except = 0;
    
 endmodule // arith_machine
