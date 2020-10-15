4e4e91c1a671cacef36e371f9fce9d40c6521c3f
Xinshuo <xinshuo3@illinois.edu>
2020-10-15 02:37:37 -0500

    finish timer.v

--- Lab8/timer.v
+++ Lab8/timer.v
@@ -11,4 +11,26 @@ module timer(TimerInterrupt, cycle, TimerAddress,
     // HINT: make your interrupt cycle register reset to 32'hffffffff
     //       (using the reset_value parameter)
     //       to prevent an interrupt being raised the very first cycle
+
+    wire TimerWrite = MemWrite && (address == 32'hffff001c);
+    wire acknowledge = MemWrite && (address == 32'hffff006c);
+    wire TimerRead = MemRead && (address == 32'hffff001c);
+
+    wire [31:0] cycle_input, cycle_output;
+    alu32 cycle_alu(cycle_input, , , `ALU_ADD, cycle_output, 32'b1);
+    register cycle_counter(cycle_output, cycle_input, clock, 1'b1, reset);
+
+    wire [31:0] interrupt_cycle_output;
+    register #(32, 32'hffffffff) interrupt_cycle(interrupt_cycle_output, data, clock, TimerWrite, reset);
+    
+    wire line_reset = acknowledge || reset;
+    wire line_enable = (interrupt_cycle_output == cycle_output);
+    register #(1) interrupt_line(TimerInterrupt, 1'b1, clock, line_enable, line_reset);
+
+    tristate read_cycle(cycle, cycle_output, TimerRead);
+
+    assign TimerAddress = (address == 32'hffff001c) || (address == 32'hffff006c);
+
+
+
 endmodule
