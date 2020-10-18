c9ecfb5e88e350ca070da02ce748912e277b10fe
Xinshuo <xinshuo3@illinois.edu>
2020-10-18 18:12:51 -0500

    finish machine.v

--- Lab8/machine.v
+++ Lab8/machine.v
@@ -18,9 +18,21 @@ module machine(clk, reset);
    wire [31:0]  rd1_data, rd2_data, B_data, alu_out_data, load_data, wr_data;
 
    //Your extra wires go here
+   wire [31:0]  rd_data;
+   wire [29:0]  EPC;
+   wire         TakenInterrupt;
+   wire         TimerInterrupt;
+   wire         TimerAddress;
+   wire         NotIO = ~TimerAddress;
+   wire [31:0]  new_wr_data;
+   wire         new_MemRead = MemRead && NotIO;
+   wire         new_MemWrite = MemWrite && NotIO;
+   wire [31:2]  ERET_mux_out;
+   wire [31:0]  interrupt_address = 32'h80000180;
+   wire [31:2]  new_next_PC;
 
 
-   register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, /* enable */1'b1, reset);
+   register #(30, 30'h100000) PC_reg(PC[31:2], new_next_PC[31:2], clk, /* enable */1'b1, reset);
    assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
    adder30 next_PC_adder(PC_plus4, PC[31:2], 30'h1);
    adder30 target_PC_adder(PC_target, PC_plus4, imm[29:0]);
@@ -33,18 +45,25 @@ module machine(clk, reset);
                       inst);
 
    regfile rf (rd1_data, rd2_data,
-               rs, rt, wr_regnum, wr_data,
+               rs, rt, wr_regnum, new_wr_data,
                RegWrite, clk, reset);
 
    mux2v #(32) imm_mux(B_data, rd2_data, imm, ALUSrc);
    alu32 alu(alu_out_data, zero, negative, ALUOp, rd1_data, B_data);
 
-   data_mem data_memory(load_data, alu_out_data, rd2_data, MemRead, MemWrite, clk, reset);
+   data_mem data_memory(load_data, alu_out_data, rd2_data, new_MemRead, new_MemWrite, clk, reset);
 
    mux2v #(32) wb_mux(wr_data, alu_out_data, load_data, MemToReg);
    mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
    
    //Connect your new modules below
    
+   timer t1(TimerInterrupt, load_data, TimerAddress, rd2_data, alu_out_data, MemRead, MemWrite, clk, reset);
+   cp0 c0(rd_data, EPC, TakenInterrupt, rd2_data, rd, next_PC, MTC0, ERET, TimerInterrupt, clk, reset);
+   mux2v #(32) MFC0_mux(new_wr_data, wr_data, rd_data, MFC0);
+   mux2v #(30) ERET_mux(ERET_mux_out, next_PC, EPC, ERET);
+   mux2v #(30) interrupt_mux(new_next_PC, ERET_mux_out, interrupt_address[31:2], TakenInterrupt);
+
+
 
 endmodule // machine
